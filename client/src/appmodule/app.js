9be103cc01e30e0323e9ed26839d46064c12ac18
Seth Koch <sethkoch1@gmail.com>
2016-12-11 11:38:42 -0600

    add directives folder and file, added a directive form-validator to validate that passwords were equal during sign upand added the directive into the singup.html template

--- client/src/appmodule/app.js
+++ client/src/appmodule/app.js
@@ -1,4 +1,9 @@
 angular.module('jamestownChicken', ['ui.router'])
-  .run(['Auth', '$window','$rootScope', function(Auth, $window, $rootScope) {
+  .run(['Auth', '$window','$rootScope', '$location', function(Auth, $window, $rootScope, location) {
     $rootScope.loggedIn = Auth.isAuthLite();
+  //   $rootScope.$on('$stateChange', function (evt, next, current) {
+  //   if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+  //     $location.path('/signin');
+  //   }
+  // });
   }])
