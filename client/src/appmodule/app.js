227d3433860abdcf6d5b90cd056910432903ba8e
Seth Koch <sethkoch1@gmail.com>
2016-12-16 09:58:26 -0600

    began to set up password reset routes, got distracted by setting up testing.  Having toubles with using the karma-mocha.

--- client/src/appmodule/app.js
+++ client/src/appmodule/app.js
@@ -1,5 +1,6 @@
 angular.module('jamestownChicken', ['ui.router'])
   .run(['Auth', '$window','$rootScope', '$location', function(Auth, $window, $rootScope, location) {
+    //determines wheter someone is logged in, but is only checking for a token present
     $rootScope.loggedIn = Auth.isAuthLite();
   //   $rootScope.$on('$stateChange', function (evt, next, current) {
   //   if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
